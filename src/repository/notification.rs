ca688920625dc749e22be4624063389894afaf9c
asteriskzie <ester.gracia.10@gmail.com>
2024-04-15 23:03:42 +0700

    Fix typo in repository/notification.rs

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -18,7 +18,7 @@ impl NotificationRepository {
         return notification; 
     }
 
-    pub fn list_all_as_string() -> vec<String> {
+    pub fn list_all_as_string() -> Vec<String> {
         return NOTIFICATIONS.read().unwrap()
             .iter().map(|f| format!("{}", f.clone())).collect(); 
     }
